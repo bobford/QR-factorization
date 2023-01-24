5d9ad4955cc08ce3e1ecd2d7992252a0b0f42bad
bobford <103234374+bobford@users.noreply.github.com>
2023-01-23 16:20:27 -0800

    Create BackgroundTask.java

---
+++ java/BackgroundTask.java
@@ -0,0 +1,35 @@
+
+package com.bob.complexqr;
+/*
+  This code was generously posted by someone who I have long ago failed to give credit to.
+*/
+import android.app.Activity;
+
+public abstract class BackgroundTask {
+
+    private final Activity activity;
+    public BackgroundTask(Activity activity) {
+        this.activity = activity;
+    }
+
+    private void startBackground() {
+        new Thread(new Runnable() {
+            public void run() {
+
+                doInBackground();
+
+                activity.runOnUiThread(new Runnable() {
+                    public void run() {
+                        onPostExecute();
+                    }
+                });
+            }
+        }).start();
+    }
+    public void execute(){
+        startBackground();
+    }
+
+    public abstract void doInBackground();
+    public abstract void onPostExecute();
+}
